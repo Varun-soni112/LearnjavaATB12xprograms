cd90ed06cad6406435fb48ed9bf7ddcf7967c2f2
Varun-soni112 <sonivarun123@gmail.com>
2025-07-17 09:27:04 +0530

    17 may 2025

--- src/Task/Task_05_TernaryOperator.java
+++ src/Task/Task_05_TernaryOperator.java
@@ -2,8 +2,8 @@ package Task;
 
 public class Task_05_TernaryOperator {
     public static void main(String[] args) {
-        int age =69;
-        String categoryOfAge=(age>18)?(age>65?"I am senior":"I am an adult"): "i can't be minor,senior or adult ";
+        int age =19;
+        String categoryOfAge=(age>18)?(age>65?"I am senior":"I am an adult"): "minor";
         System.out.println(categoryOfAge);
 
     }
