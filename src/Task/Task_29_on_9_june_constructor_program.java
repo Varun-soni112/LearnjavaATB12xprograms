8bd4e8b3cb0affe2e3a077620bf2fc5986599a3c
varun soni <sonivarun123@gmail.com>
2025-09-04 13:59:44 +0530

    task on 9th june

---
+++ src/Task/Task_29_on_9_june_constructor_program.java
@@ -0,0 +1,25 @@
+package Task;
+
+public class Task_29_on_9_june_constructor_program {
+    public static void main(String[] args) {
+     car c= new car("maruti", 2025,250000);
+        System.out.println(c.brand);
+        System.out.println(c.model);
+        System.out.println(c.price);
+    }
+}
+class car
+{
+    String brand;
+    int model;
+    long price;
+
+    car(String brand, int model , long price)
+    {
+        this.brand=brand;
+        this.model=model;
+        this.price=price;
+
+    }
+
+}
