9804ac48c88f034ac68fb1b1d90b9f5e90eafdde
varun soni <sonivarun123@gmail.com>
2025-11-04 08:31:27 +0530

    25 may task

--- src/Task/_25may/Task_16_on_25may_prime_number.java
+++ src/Task/_25may/Task_16_on_25may_prime_number.java
@@ -1,14 +1,17 @@
 package Task._25may;
 import java.util.Scanner;
-public class Task_16_on_25may_prime_number {
-    public static void main(String[] args) {
+public class Task_16_on_25may_prime_number
+{
+    public static void main(String[] args)
+    {
         Scanner s= new Scanner(System.in);
         System.out.println("enter your number ");
         int number= s.nextInt();
         int count=0;
         if(number<=1)
         {
-            //System.out.println("that num isn't prime ");
+           System.out.println("that num isn't prime ");
+            return;
         }
         else
         {
@@ -16,12 +19,10 @@ public class Task_16_on_25may_prime_number {
             {
                 if(number%i ==0)
                 {
-                    //System.out.println("your number is prime");
+//                    System.out.println("your number is prime");
                     count++;
                 }
             }
-
-
         }
             if(count==2)
             {
@@ -33,9 +34,9 @@ public class Task_16_on_25may_prime_number {
                 System.out.println("your number isn't prime");
             }
 
-            }
-
-
     }
+
+
+}
 
 
